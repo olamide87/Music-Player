f85297c301024597e73ee0fdcf0e6b5cb665c456
olamide oladinni <olajide1987@gmail.com>
2021-05-18 02:17:02 -0500

    setup

---
+++ src/components/Player.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Player (props) {
+  return (
+    <div className='c-player'>
+      <audio></audio>
+      <h4>Playing Now</h4>
+      {/* {DETAILS} */}
+      {/* {CONTROLS} */}
+      <p><strong>Next up:</strong> </p>
+    </div>
+  )
+}
+
+export default Player;
